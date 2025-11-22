2baa1a7a334050975b67a740a81afb041b78e281
beatrizbrito314 <emillybea314@gmail.com>
2025-11-22 00:46:09 -0300

    Configuracao do Maven. SUPOSTAMENTE esta funcionando

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,13 @@
 package application;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) {
-        Label label = new Label("JavaFX 25 está funcionando!");
-        Scene scene = new Scene(label, 300, 200);
-        stage.setScene(scene);
+        stage.setTitle("COSMO");
         stage.show();
     }
 
